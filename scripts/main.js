4c6954e245b5f917adacd5cb01aebfab8495b30c
FBGFi <50401282+FBGFi@users.noreply.github.com>
2020-04-04 00:11:36 +0300

    asd

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
-
+let clickableNumbers = document.getElementsByClassName('clickablenumbers');
+let cells;
+let cellDiv;
+//console.log(clickableNumbers);
+for (let i = 0; i < clickableNumbers.length; i++) {
+    cells = clickableNumbers[i].cells;
+    for (let j = 0; j < cells.length; j++) {
+        cellDiv = cells[j].firstChild;
+        //console.log(cells[j].firstChild.tagName);
+        if(cellDiv != null && cellDiv.tagName == "DIV"){        
+            cellDiv.addEventListener("click", (e) => {
+                e.toElement.classList.toggle("clicked");              
+            });        
+        }   
+    }
+    
+}
